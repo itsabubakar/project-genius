dd6c3d72d81bceaffef9106be3d41a4c29d52547
Hamid-Usman <abdulhamidusman218@gmail.com>
2025-03-10 21:43:08 +0100

    fixed query param error

--- app/components/application/button.js
+++ app/components/application/button.js
@@ -1,7 +1,7 @@
 export default function Button({onClick, children, type, disabled, className}) {
     return (
         <button disabled={disabled} onClick={onClick} type={type} className={`sm:ml-auto w-full sm:w-[314px] text-white px-5 py-3 rounded-full mt-2 ${className} ${disabled === true ? 'bg-primary_surface' : 'bg-primary'}`}>
-            {children}
-        </button>
+                {children
+        }</button>
     )
 }
