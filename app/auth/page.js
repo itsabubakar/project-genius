1582fe71ba295135ab2bdba1d074bb189722af1c
Hamid-Usman <abdulhamidusman218@gmail.com>
2025-03-29 11:46:01 +0100

    login state management completed

--- app/auth/page.js
+++ app/auth/page.js
@@ -16,105 +16,105 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { motion, AnimatePresence} from "framer-motion"
 import slideUp from "../motion/slideUp";
 import useLoginStore from "../store/loginStore";
+import { useLogin } from "../api/login";
 
 // Zod schema for validation
 export const loginSchema = yup.object().shape({
-  email: yup.string().email("Invalid email").required("Please enter a valid email address"),
-  password: yup.string().min(8, "Password must be at least 8 characters")
+	email: yup.string().email("Invalid email").required("Please enter a valid email address"),
+	password: yup.string().min(8, "Password must be at least 8 characters")
 });
 
 export default function Login() {
-  const router = useRouter();
-  const { login, loading, loginError } = useLoginStore()
-  
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL_DEV;
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL_DEV;
+    const router = useRouter();
+	const { mutate, isPending, error } = useLogin(apiUrl);
+    
+    const navigateTocreate = () => {
+    	router.push('/auth/create-profile');
+    };
 
-  const navigateTocreate = () => {
-    router.push('/auth/create-profile');
-  };
+    const {
+		register,
+		handleSubmit,
+		formState: { errors},
+    } = useForm({
+		resolver: yupResolver(loginSchema),
+		mode: "onChange",
+    });
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors},
-  } = useForm({
-    resolver: yupResolver(loginSchema),
-    mode: "onChange",
-  });
+    const onSubmit = (data) => {
+        mutate(data, apiUrl, router)
+    }
 
-  const onSubmit = (data) => {
-    login(data, apiUrl, router)
-  }
+    return (
+        <AuthLayout>
+			<motion.form
+			initial="initial"
+			animate="animate"
+			variants={slideUp}
+			transition={{duration: 0.8}}
+			onSubmit={handleSubmit(onSubmit)} className="flex flex-col xs:items-center gap-4">
+				<Heading
+				heading={"Let's continue building"}
+				subHeading={"Log in to pick up where you left off"}
+				classname={"gap-2"}
+			/>
 
-  return (
-    <AuthLayout>
-      <motion.form
-      initial="initial"
-      animate="animate"
-      variants={slideUp}
-      transition={{duration: 0.8}}
-      onSubmit={handleSubmit(onSubmit)} className="flex flex-col xs:items-center gap-4">
-        <Heading
-          heading={"Let's continue building"}
-          subHeading={"Log in to pick up where you left off"}
-          classname={"gap-2"}
-        />
+			<div className="flex flex-col gap-2 text-greyscale_text">
+				<label htmlFor="email">Email address</label>
+				<input
+					type="email"
+					id="email"
+					{...register("email")}
+					className={`inter w-[330px] sm:w-[340px] md:w-[360px]  px-4 py-3 rounded-xl outline-none transition-all 
+					${
+					errors.email
+						? "outline-error_dark text-error_dark bg-error_subtle"
+						:  ""
+					}`}
+					placeholder="Enter your email address"
+				/>
+				{errors.email && (
+				<p className="text-red-500 text-sm">{errors.email.message}</p>
+				)}
+			</div>
 
-        <div className="flex flex-col gap-2 text-greyscale_text">
-          <label htmlFor="email">Email address</label>
-          <input
-            type="email"
-            id="email"
-            {...register("email")}
-            className={`inter w-[330px] sm:w-[340px] md:w-[360px]  px-4 py-3 rounded-xl outline-none transition-all 
-              ${
-                errors.email
-                  ? "outline-error_dark text-error_dark bg-error_subtle"
-                  :  ""
-              }`}
-            placeholder="Enter your email address"
-          />
-          {errors.email && (
-            <p className="text-red-500 text-sm">{errors.email.message}</p>
-          )}
-        </div>
+			<div className="flex flex-col gap-2 text-greyscale_text">
+				<label htmlFor="password">Password</label>
+				<input
+					type="password"
+					id="password"
+					{...register("password")}
+					className={`inter w-[330px] sm:w-[340px] md:w-[360px] px-4 py-3 rounded-xl outline-none transition-all 
+					${
+					errors.password
+						? "outline-error_dark bg-error_subtle"
+						: ""
+					}`}
+					placeholder="Enter your password"
+				/>
+				{errors.password && (
+				<p className="text-red-500 text-sm">{errors.password.message}</p>
+				)}
+			</div>
 
-        <div className="flex flex-col gap-2 text-greyscale_text">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            {...register("password")}
-            className={`inter w-[330px] sm:w-[340px] md:w-[360px] px-4 py-3 rounded-xl outline-none transition-all 
-              ${
-                errors.password
-                  ? "outline-error_dark bg-error_subtle"
-                  : ""
-              }`}
-            placeholder="Enter your password"
-          />
-          {errors.password && (
-            <p className="text-red-500 text-sm">{errors.password.message}</p>
-          )}
-        </div>
+			{error && (
+				<p className="text-red-500 text-sm">{error}</p>
+			)}
 
-        {loginError && (
-          <p className="text-red-500 text-sm">{loginError}</p>
-        )}
+			<div className="flex flex-col gap-4 w-[100%] sm:flex-row justify-center items-center">
+				<ButtonBlue classname={"active:bg-greyscale_subtitle md:w-[50%] flex justify-center"}>
+				{isPending ? <Image src={spinner} className="animate-spin"/> : "Login"}
+				</ButtonBlue>
 
-        <div className="flex flex-col gap-4 w-[100%] sm:flex-row justify-center items-center">
-          <ButtonBlue classname={"active:bg-greyscale_subtitle md:w-[50%] flex justify-center"}>
-            {loading ? <Image src={spinner} className="animate-spin"/> : "Login"}
-          </ButtonBlue>
-
-          <ButtonGlass classname="md:w-[50%]" onClick={navigateTocreate}>
-            Register
-          </ButtonGlass>
-        </div>
-        <Link href={'/auth/forgot-password'} className="text-right md:ml-auto text-greyscale_text">
-          Forgot Password?
-        </Link>
-      </motion.form>
+				<ButtonGlass classname="md:w-[50%]" onClick={navigateTocreate}>
+				Register
+				</ButtonGlass>
+			</div>
+			<Link href={'/auth/forgot-password'} className="text-right md:ml-auto text-greyscale_text">
+				Forgot Password?
+			</Link>
+			</motion.form>
     </AuthLayout>
-  );
+    );
 }
--- app/store/loginStore.js
+++ app/store/loginStore.js
@@ -1,36 +1,9 @@
 import { create } from "zustand";
 
-const useLoginStore = create((set) => ({
-    user: null,
-    loading:false,
-    loginError: null,
-    login: async (data, apiUrl, router) => {
-        set({loading: true, error: ""})
-        try {
-          const response = await fetch(`${apiUrl}/auth/connect/`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(data),
-            });
-    
-            const result = await response.json();
-            console.log(result);
-    
-            if (response.ok) {
-                localStorage.setItem("user", JSON.stringify(result));
-                set({user: result})
-                router.push('/dashboard');
-            } else {
-                set({loginError: result.error || "Invalid login credentials"});
-            }
-        } catch (error) {
-            set({loginError: "Network error, please try again."});
-        } finally {
-            set({loading: false})
-        }
+export const loginStore = create((set) => ({
+    user: JSON.parse(localStorage.getItem("user")),
+    setUser: (user) => {
+        set({ user });
+        localStorage.setItem("user", JSON.stringify(user));
     },
-}))
-
-export default useLoginStore
+}));
