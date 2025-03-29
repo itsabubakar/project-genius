5290c0b43d68d09449340f848092d59f0f17a54e
Hamid-Usman <abdulhamidusman218@gmail.com>
2025-03-29 15:26:37 +0100

    updated modal component

--- app/dashboard/components/modal.js
+++ app/dashboard/components/modal.js
@@ -1,9 +1,13 @@
 import Backdrop from "@/app/modals/backdrop";
-
+import { motion } from "framer-motion";
 export default function Modal({ heading, subHeading, modalClose, children}) {
     return (
         <Backdrop onClose={modalClose}>
-            <div
+            <motion.div
+            initial={{ opacity: 0, x: "50vw"}}
+            animate={{ opacity: 1, x: 0}}
+            exit={{ opacity: 0, x: "-50vw" }}
+            transition={{duration: 0.8}}
             onClick={(e) => e.stopPropagation()}
                 className="px-4 py-6  md:px-6 flex flex-col gap-2 mx-5 rounded-2xl
                     bg-greyscale_background_light text-center items-center
@@ -12,7 +16,7 @@ export default function Modal({ heading, subHeading, modalClose, children}) {
                 <h2 className="text-center text-2xl font-bold">{heading}</h2>
                 <p className="inter text-center text-greyscale_text">{subHeading}</p>
                 {children}
-            </div>
+            </motion.div>
         </Backdrop>
     );
 }
--- app/store/useDashboardStore.js
+++ app/store/useDashboardStore.js
@@ -2,6 +2,7 @@ import { create } from "zustand";
 
 const useDashboardStore = create((set) => ({
   userData: null,
+  setUserData: (data) => set({ userData: data }),
 }));
 
 export default useDashboardStore;
